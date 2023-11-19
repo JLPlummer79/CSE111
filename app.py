3df696b5e64b363f4bc361ba567fa4d5bf7a1d64
Jesse <bfantastic@gmail.com>
2023-11-19 12:42:39 -0800

    All tables frm project.db added to app

--- app.py
+++ app.py
@@ -31,3 +31,32 @@ class Location(project.Model):
     city = project.Column(project.String(45))
     zipcode = project.Column(project.Int(14))
       
+class Permits(project.Model):
+    parkIDNumber = project.Column(project.Int(15), primary_key = True)
+    IdNumber = project.Column(project.Int(20), primary_key = True)
+    ownerName = project.Column(project.String(45))
+    type = project.Column(project.String(25))
+    duration = project.Column(project.Int(3))
+    startDate = project.Column(project.String(25))
+    endDate = project.Column(project.String(25))
+
+class Fees(project.Model):
+     parkIDNumber = project.Column( project.Int(15), primary_key = True)
+     permitType = project.Column(project.String(24), primary_key = True)
+     amount = project.Column(project.Float(15))
+
+class Recreation(project.Model):
+    parkIDNumber = project.Column(project.Int(15), primary_key = True)
+    permitIDNumber = project.Column(project.Int(20), primay_key = True) 
+    activity = project.Column(project.String(25))
+    trailName = project.Column(project.String(50))
+    trailHead = project.Column(project.String(50))
+    road = project.Column(project.String(50))
+    campsiteName = project.Column(project.String(50))
+
+class Staff(project.Model):
+    employeeIDNumber = project.Column(project.Int(20), primary_key = True)
+    parkIDNumber = project.Colum(project.Int(15), primary_key = True)
+    department = project.Column(project.String(20))
+    schedule = project.Column(project.String(50))
+    name = project.Column(project.String(50))
